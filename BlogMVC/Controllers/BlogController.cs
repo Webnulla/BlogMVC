92a572b7dbc9bfc7e1d1ecda62575c8f393ec81c
Konstantin <muselsps@ya.ru>
2022-07-26 20:35:58 +0300

    Blog

---
+++ BlogMVC/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using BlogMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMVC.Controllers;
+
+public class BlogController : Controller
+{
+    static List<BlogEntry> Posts = new List<BlogEntry>();
+    
+    public IActionResult CreatorPage(Guid id)
+    {
+        if (id != Guid.Empty)
+        {
+            BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == id);
+
+            return View(model: existingEntry);
+        }
+        return View();
+    }
+    
+    public IActionResult Index()
+    {
+        return View("Index", Posts);
+    }
+
+    [HttpPost]
+    public IActionResult CreatorPage(BlogEntry entry)
+    {
+        if (entry.Id == Guid.Empty)
+        {
+            BlogEntry blogEntry = new BlogEntry();
+            blogEntry.Content = entry.Content;
+            blogEntry.Id = Guid.NewGuid();
+            Posts.Add(blogEntry);
+        }
+        else
+        {
+            BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == entry.Id);
+            existingEntry.Content = entry.Content;
+        }
+        
+        return RedirectToAction("Index");
+    }
+}
---
+++ BlogMVC/Models/BlogEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogMVC.Models;
+
+public class BlogEntry
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+}
